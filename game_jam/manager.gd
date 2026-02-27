182819c5a295aa9e1ccb32e2870a919a44815286
Froyzz <santiago.sciarra16@gmail.com>
2026-02-27 10:17:33 +0100

    made quit

--- game_jam/manager.gd
+++ game_jam/manager.gd
@@ -10,3 +10,5 @@ func _ready() -> void:
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("restart"):
 		get_tree().reload_current_scene()
+	if Input.is_action_just_pressed("quit"):
+		get_tree().quit()
